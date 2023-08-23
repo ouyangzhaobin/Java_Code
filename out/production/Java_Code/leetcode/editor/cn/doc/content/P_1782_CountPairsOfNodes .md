de5f895770b828ff8d915622c35785aa152c2bda
ouyang <ouyangzhaobin6@163.com>
2023-08-23 16:15:07 +0800

    8月23日 LeetCode完成1782、88题

---
+++ out/production/Java_Code/leetcode/editor/cn/doc/content/P_1782_CountPairsOfNodes .md
@@ -0,0 +1,44 @@
+<p>给你一个无向图，无向图由整数&nbsp;<code>n</code>&nbsp;&nbsp;，表示图中节点的数目，和&nbsp;<code>edges</code>&nbsp;组成，其中&nbsp;<code>edges[i] = [u<sub>i</sub>, v<sub>i</sub>]</code>&nbsp;表示&nbsp;<code>u<sub>i</sub></code> 和&nbsp;<code>v<sub>i</sub></code><sub>&nbsp;</sub>之间有一条无向边。同时给你一个代表查询的整数数组&nbsp;<code>queries</code>&nbsp;。</p>
+
+<p>第 <code>j</code> 个查询的答案是满足如下条件的点对 <code>(a, b)</code> 的数目：</p>
+
+<ul> 
+ <li><code>a &lt; b</code></li> 
+ <li><code>cnt</code>&nbsp;是与 <code>a</code>&nbsp;<strong>或者&nbsp;</strong><code>b</code>&nbsp;相连的边的数目，且 <code>cnt</code>&nbsp;<strong>严格大于&nbsp;</strong><code>queries[j]</code>&nbsp;。</li> 
+</ul>
+
+<p>请你返回一个数组&nbsp;<code>answers</code>&nbsp;，其中&nbsp;<code>answers.length == queries.length</code> 且&nbsp;<code>answers[j]</code>&nbsp;是第 <code>j</code>&nbsp;个查询的答案。</p>
+
+<p>请注意，图中可能会有 <strong>重复边</strong>&nbsp;。</p>
+
+<p>&nbsp;</p>
+
+<p><strong>示例 1：</strong></p> 
+<img alt="" src="https://pic.leetcode-cn.com/1614828447-GMnLVg-image.png" style="width: 310px; height: 278px;" /> 
+<pre>
+<b>输入：</b>n = 4, edges = [[1,2],[2,4],[1,3],[2,3],[2,1]], queries = [2,3]
+<b>输出：</b>[6,5]
+<b>解释：</b>每个点对中，与至少一个点相连的边的数目如上图所示。
+</pre>
+
+<p><strong>示例 2：</strong></p>
+
+<pre>
+<b>输入：</b>n = 5, edges = [[1,5],[1,5],[3,4],[2,5],[1,3],[5,1],[2,3],[2,5]], queries = [1,2,3,4,5]
+<b>输出：</b>[10,10,9,8,6]
+</pre>
+
+<p>&nbsp;</p>
+
+<p><strong>提示：</strong></p>
+
+<ul> 
+ <li><code>2 &lt;= n &lt;= 2 * 10<sup>4</sup></code></li> 
+ <li><code>1 &lt;= edges.length &lt;= 10<sup>5</sup></code></li> 
+ <li><code>1 &lt;= u<sub>i</sub>, v<sub>i</sub> &lt;= n</code></li> 
+ <li><code>u<sub>i </sub>!= v<sub>i</sub></code></li> 
+ <li><code>1 &lt;= queries.length &lt;= 20</code></li> 
+ <li><code>0 &lt;= queries[j] &lt; edges.length</code></li> 
+</ul>
+
+<div><div>Related Topics</div><div><li>图</li><li>数组</li><li>双指针</li><li>二分查找</li><li>排序</li></div></div><br><div><li>👍 83</li><li>👎 0</li></div>
---
+++ src/leetcode/editor/cn/P_88_MergeSortedArray .java
@@ -0,0 +1,128 @@
+package leetcode.editor.cn;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+  * 题目Id：88; 
+  * 题目：合并两个有序数组，merge-sorted-array; 
+  * 日期：2023-08-23 12:21:21
+*/
+
+//给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。 
+//
+// 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。 
+//
+// 注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并
+//的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+//输出：[1,2,2,3,5,6]
+//解释：需要合并 [1,2,3] 和 [2,5,6] 。
+//合并结果是 [1,2,2,3,5,6] ，其中斜体加粗标注的为 nums1 中的元素。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums1 = [1], m = 1, nums2 = [], n = 0
+//输出：[1]
+//解释：需要合并 [1] 和 [] 。
+//合并结果是 [1] 。
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums1 = [0], m = 0, nums2 = [1], n = 1
+//输出：[1]
+//解释：需要合并的数组是 [] 和 [1] 。
+//合并结果是 [1] 。
+//注意，因为 m = 0 ，所以 nums1 中没有元素。nums1 中仅存的 0 仅仅是为了确保合并结果可以顺利存放到 nums1 中。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// nums1.length == m + n 
+// nums2.length == n 
+// 0 <= m, n <= 200 
+// 1 <= m + n <= 200 
+// -10⁹ <= nums1[i], nums2[j] <= 10⁹ 
+// 
+//
+// 
+//
+// 进阶：你可以设计实现一个时间复杂度为 O(m + n) 的算法解决此问题吗？ 
+//
+// Related Topics 数组 双指针 排序 👍 2069 👎 0
+
+
+class P_88_MergeSortedArray{
+    public static void main(String[] args) {
+        Solution solution = new P_88_MergeSortedArray().new Solution();
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+        int[] nums1 = new int[m+n];
+        for (int i = 0; i < m+n; i++) {
+            nums1[i] = scanner.nextInt();
+        }
+
+        int[] nums2 = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums2[i] = scanner.nextInt();
+        }
+
+        solution.merge(nums1,m,nums2,n);
+
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // 时间复杂度为m+n
+        int ans1 = 0, ans2 = m+n;
+        for (int i = m-1; i >= 0; i--) {
+            if(n==0){
+                break;
+            }
+            if(nums1[i] > nums2[n-1]){
+                nums1[ans2-1-ans1] = nums1[i];
+            }else{
+                nums1[ans2-1-ans1] = nums2[n-1];
+                n--;
+                i++;
+            }
+            ans1+=1;
+        }
+        if(n!=0){
+            for (int i = 0; i < n; i++) {
+                nums1[i] = nums2[i];
+            }
+        }
+
+        /*
+        // 时间复杂度为nlogn
+        if(nums1 != null){
+            for (int i = m; i < m+n ; i++) {
+                nums1[i] = nums2[i-m];
+            }
+        }
+        // sort的平均时间复杂度为O（nlogn)
+        Arrays.sort(nums1);
+         */
+
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
